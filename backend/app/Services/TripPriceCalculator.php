792c8b23fdaa00366008156f86a19f48b2813e8d
Ruslan 'Kolm-dev' Soloviov <fehax@icloud.com>
2026-09-26 15:17:25 +0300

    Optimized and round calculated trip price to x.xx

--- backend/app/Services/TripPriceCalculator.php
+++ backend/app/Services/TripPriceCalculator.php
@@ -6,18 +6,18 @@ use App\Models\PricingSetting;
 
 class TripPriceCalculator
 {
-    public function calculate(float $distance)
+    public function calculate(float $distance): float
     {
         $priceSetting = PricingSetting::firstOrFail();
-        $pricePerKm = $priceSetting->price_per_km;
-        $basePrice = $priceSetting->base_price;
-        $minimumPrice = $priceSetting->minimum_price;
-        $recomendedPrice = $basePrice + ($distance * $pricePerKm);
-        if ($recomendedPrice >= $minimumPrice) {
-            return $recomendedPrice;
-        }
 
-        return $minimumPrice;
+        $recommendedPrice = $priceSetting->base_price
+            + ($distance * $priceSetting->price_per_km);
 
+        $finalPrice = max(
+            $recommendedPrice,
+            $priceSetting->minimum_price,
+        );
+
+        return round((float) $finalPrice, 2);
     }
 }
